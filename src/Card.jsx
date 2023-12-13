e99688bd8f1854936cc39291d8f646f0006759c1
Sumit Kumar Sah <sumitedu4@gmail.com>
2023-12-13 15:02:06 +0530

    bug solved

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 
-const Card=(name, id, email)=>{
+const Card=({id,name,email,username})=>{
     return(
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={'https://robohash.org/${id}?200*200'} alt="photo" />
+            <img src={`https://robohash.org/${id}?200*200`}/>
             <div>
-                <h1>{name}</h1>
+                <p>{name}</p>
                 <p>{email}</p>
+                <p>{username}</p>
             </div>
         </div>
     );
 }
 
-export default Card;
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Card from './Card.jsx'
-//import 'tachyons';
+import Card from './Card';
+import 'tachyons';
 
-import {robots} from './robots';
+import {Robots} from './Robots';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} username={robots[4].username}/>
-    <h1>kbjabfdsujkvbj</h1>
+    <Card id={Robots[0].id} name={Robots[0].name} email={Robots[0].email} username={Robots[0].username}/>
+    <Card id={Robots[1].id} name={Robots[1].name} email={Robots[1].email} username={Robots[1].username}/>
+    <Card id={Robots[2].id} name={Robots[2].name} email={Robots[2].email} username={Robots[2].username}/>
+    <Card id={Robots[3].id} name={Robots[3].name} email={Robots[3].email} username={Robots[3].username}/>
+    <Card id={Robots[4].id} name={Robots[4].name} email={Robots[4].email} username={Robots[4].username}/>
   </React.StrictMode>,
 )
 
